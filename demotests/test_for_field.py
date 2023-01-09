a0cae85596211684de0a3923bc333b2ce47cce5c
Versonya <elsidor80@gmail.com>
2023-01-09 20:48:42 +0200

    Test for field

--- demotests/test_for_field.py
+++ demotests/test_for_field.py
@@ -10,26 +10,15 @@ from selenium_base.base import SeleniumBase
 class TestForField():
 
 
-        def test_for_field(self):
-                driver = Elements_sign(self.driver)
-                name_field = driver.find_full_name_field()
-                name_field.send_keys('Elena')
-                email_field = driver.find_email_field()
-                email_field.send_keys('eps@gmail.com')
-                current_address_field = driver.find_current_address_field()
-                current_address_field.send_keys('Ukraine')
-                permanent_address_field = driver.find_permanent_address_field()
-                permanent_address_field.send_keys('Kharkiv')
-                submit_button = driver.find_submit_button()
-                submit_button.click()
+        def test_text_box(self):
+                text_box_page = Elements_sign(self.driver)
+                full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+                output_name, output_email, output_current_address, output_permanent_address = text_box_page.entered_data()
+                assert full_name == output_name
+                assert email == output_email
+                assert current_address == output_current_address
+                assert permanent_address == output_permanent_address
                 time.sleep(5)
-                output_name = driver.get_output_name().split(':', maxsplit=1)[1]
-                output_email = driver.get_output_email().split(':', maxsplit=1)[1]
-                output_current_address = driver.get_output_current_address().split(':', maxsplit=1)[1]
-                output_permanent_address = driver.get_output_permanent_address().split(':', maxsplit=1)[1]
 
-                actual_result = list['Elena', 'eps@gmail.com', 'Ukraine', 'Kharkiv']
-                expected_result = list[output_name, output_email, output_current_address, output_permanent_address]
-                assert actual_result == expected_result 
-                #print(output_name, output_email, output_current_address, output_permanent_address)
+               #print(output_name, output_email, output_current_address, output_permanent_address)
 
